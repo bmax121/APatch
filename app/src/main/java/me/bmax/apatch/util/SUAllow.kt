df4c9fab87f598707ed8bf3df7437d6f6ca936b2
bmax <bmax@placeholder.com>
2023-12-25 07:46:47 +0800

    temp

--- app/src/main/java/me/bmax/apatch/util/SUAllow.kt
+++ app/src/main/java/me/bmax/apatch/util/SUAllow.kt
@@ -1,6 +1,5 @@
 package me.bmax.apatch.util
 
-import android.util.Log
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import me.bmax.apatch.APApplication
@@ -13,7 +12,6 @@ import kotlin.concurrent.thread
 object SuConfig {
     private val mutex = Mutex()
     private val TAG = "SUAllowConfig"
-
 
     suspend fun removePackage(packageName: String) {
         mutex.withLock {
@@ -45,7 +43,7 @@ object SuConfig {
             thread {
                 val allows = HashMap<String, Natives.Profile>()
                 Natives.su()
-                val file = File(APApplication.SU_PATH_FILE)
+                val file = File(APApplication.ALLOW_UID_FILE)
                 if(file.exists()) {
                     file.readLines().filter { !it.isEmpty() }.forEach {
                         val p = Natives.Profile.from(it)
@@ -55,7 +53,6 @@ object SuConfig {
                     if(!file.parentFile.exists()) file.parentFile.mkdirs()
                 }
                 allows[profile.pkg] = profile
-                Log.d(TAG, allows.toString())
                 BufferedWriter(FileWriter(file, false)).use { writer ->
                     allows.values.forEach {
                         writer.write(it.toLine())
