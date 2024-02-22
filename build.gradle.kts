bc266e3cca059ea4a4a38d199aedfc681df01e16
GarfieldHan <2652609017@qq.com>
2024-02-22 09:31:29 +0800

    project: Upgrade agp and dependences
    Test: Build, Start, Patch Boot Image
    
    Signed-off-by: GarfieldHan <2652609017@qq.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,8 +35,8 @@ cmaker {
 project.ext.set("kernelPatchVersion", "0.9.1-dev")
 
 val androidMinSdkVersion = 26
-val androidTargetSdkVersion = 33
-val androidCompileSdkVersion = 33
+val androidTargetSdkVersion = 34
+val androidCompileSdkVersion = 34
 
 val androidBuildToolsVersion = "33.0.2"
 val androidCompileNdkVersion = "25.2.9519653"
