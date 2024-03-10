bmax121/APatch

commit 6d442a652830b6a2e6d987c370f650f7465e5bf6

PatchesViewModel: Fix A-only devices update
Signed-off-by: GarfieldHan <2652609017@qq.com>

app/src/main/java/me/bmax/apatch/ui/viewmodel/PatchesViewModel.kt
@@ -197,7 +197,11 @@ class PatchesViewModel : ViewModel() {
             "sh boot_extract.sh",
         )
         if(result.isSuccess) {
-            bootSlot = result.out.filter { it.startsWith("SLOT=") }[0].removePrefix("SLOT=")
+            if (!result.out.contains("SLOT=")) {
+                bootSlot = ""
+            } else {
+                bootSlot = result.out.filter { it.startsWith("SLOT=") }[0].removePrefix("SLOT=")
+            }
             bootDev = result.out.filter { it.startsWith("BOOTIMAGE=") }[0].removePrefix("BOOTIMAGE=")
             Log.d(TAG, "current slot: ${bootSlot}")
             Log.d(TAG, "current bootimg: ${bootDev}")

app/src/main/assets/util_functions.sh
@@ -69,6 +69,16 @@ mount_partitions() {
   [ -z $SLOT ] || echo "SLOT=$SLOT"
 }
 
+exchange_boot_slot(){  
+  if [[ $SLOT == *_a ]]; then
+    SLOT='_b'
+  fi
+
+  if [[ $SLOT == *_b ]]; then
+    SLOT='_a'
+  fi
+}
+
 find_boot_image() {
   if [ ! -z $SLOT ]; then
     BOOTIMAGE=$(find_block "boot$SLOT")

commit 98e1778979a6a773ef0ccf01eceb9cab23d0bc07

PatchesViewModel: Fix slot is null
The bug was introduced from 6d442a6.
result.out is List<String> but not String, directly using "contains" makes
bootSlot always null.

Signed-off-by: GarfieldHan <2652609017@qq.com>

app/src/main/java/me/bmax/apatch/ui/viewmodel/PatchesViewModel.kt
@@ -196,11 +196,12 @@ class PatchesViewModel : ViewModel() {
             "cd $patchDir",
             "sh boot_extract.sh",
         )
-        if(result.isSuccess) {
-            if (!result.out.contains("SLOT=")) {
-                bootSlot = ""
+
+        if (result.isSuccess) {
+            bootSlot = if (!result.out.toString().contains("SLOT=")) {
+                ""
             } else {
-                bootSlot = result.out.filter { it.startsWith("SLOT=") }[0].removePrefix("SLOT=")
+                result.out.filter { it.startsWith("SLOT=") }[0].removePrefix("SLOT=")
             }
             bootDev = result.out.filter { it.startsWith("BOOTIMAGE=") }[0].removePrefix("BOOTIMAGE=")
             Log.d(TAG, "current slot: $bootSlot")