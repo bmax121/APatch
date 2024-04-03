f8aab71217149ba34cff2ec9b293fad8a6343400
GarfieldHan <2652609017@qq.com>
2024-04-03 16:53:04 +0800

    Revert "kp: bump to 0.10.5-dev"
    This reverts commit 4b9034d50ad86988c33fa045fa160134fbbb76c0.
    
    Signed-off-by: GarfieldHan <2652609017@qq.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,8 @@ cmaker {
     }
 }
 
-project.ext.set("kernelPatchVersion", "0.10.5-dev")
+project.ext.set("kernelPatchVersion", "0.10.4")
+
 
 val androidMinSdkVersion = 26
 val androidTargetSdkVersion = 34
