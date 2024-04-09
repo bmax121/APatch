064f079eccfa852f17ca22fdfac8921635ea296d
bmax <bmax@placeholder.com>
2024-04-09 18:01:10 +0800

    kp: test update kp to 0.10.6-dev, compat compat-syscall, 32-bits su command is support now
    Signed-off-by: GarfieldHan <2652609017@qq.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ cmaker {
     }
 }
 
-project.ext.set("kernelPatchVersion", "0.10.5")
+project.ext.set("kernelPatchVersion", "0.10.6-dev")
 
 val androidMinSdkVersion = 26
 val androidTargetSdkVersion = 34
