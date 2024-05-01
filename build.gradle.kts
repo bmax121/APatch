3c92064fc3ed947dea973613398ffa690c60a119
GarfieldHan <2652609017@qq.com>
2024-05-01 09:28:30 +0800

    kp: Bump to 0.10.7
    Signed-off-by: GarfieldHan <2652609017@qq.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ cmaker {
     }
 }
 
-project.ext.set("kernelPatchVersion", "0.10.6-dev")
+project.ext.set("kernelPatchVersion", "0.10.7")
 
 val androidMinSdkVersion = 26
 val androidTargetSdkVersion = 34
