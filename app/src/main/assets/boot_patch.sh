d6e6d41d5d7d95bf4a5d4ca38107fb31d09cad2a
SakuraKyuo <sakurakyuo@protonmail.com>
2024-08-04 17:19:33 +0800

    scripts: Fixed detecting kernel configuration in APP
    In commit https://github.com/bmax121/APatch/commit/6d62eaebcf6fdfa08253da2386d2b17557bbce24, we used mktemp provided by toybox to avoid the problem of missing mktemp. However, the test found that permission denied would occur on some devices because mktemp provided by toybox would create temporary files in /tmp, which is not possible in non-Recovery. Therefore, we added the parameter of passing in the APP cache directory to the APP patch script as the directory for mktemp to generate temporary files. This parameter is not passed in when flashing in Recovery mode.
    
    Test: Build Manager - Patch in the device that not have mktemp - Check result
    
    Result: It's working fine now(Tested on Redmi Note 11 5G)
    
    Signed-off-by: SakuraKyuo <SakuraKyuo@protonmail.com>

--- app/src/main/assets/boot_patch.sh
+++ app/src/main/assets/boot_patch.sh
@@ -49,7 +49,7 @@ echo "- Unpacking boot image"
   fi
 fi
 
-if [ ! $(sh extract-ikconfig kernel | grep CONFIG_KALLSYMS=y) ]; then
+if [ ! $(sh extract-ikconfig kernel "/data/user/0/me.bmax.apatch/cache/" | grep CONFIG_KALLSYMS=y) ]; then
 	echo "- Patcher has Aborted!"
 	echo "- APatch requires CONFIG_KALLSYMS to be Enabled."
 	echo "- But your kernel seems NOT enabled it."
