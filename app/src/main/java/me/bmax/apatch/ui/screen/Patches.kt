93154859b69e588f413adff397a80e7c45598987
SakuraKyuo <sakurakyuo@protonmail.com>
2024-08-06 04:28:11 +0800

    Manager: Add SuperKey special character detection
    When patching in APP, users usually use special characters as SuperKey for security reasons. However, since users often use special characters that may cause program errors as SuperKey, special character detection is added.
    
    While ensuring that most special characters can still be used as SuperKey normally, the special character detection added this time only includes $ " ' [ ], which are special symbols that are easy to cause program errors. Other special symbols such as @ # are not affected.
    
    Test: Build Manager - Patch kernel with using special character that not allow - Patch won't start and throw error note
    
    Signed-off-by: SakuraKyuo <sakurakyuo@protonmail.com>

--- app/src/main/java/me/bmax/apatch/ui/screen/Patches.kt
+++ app/src/main/java/me/bmax/apatch/ui/screen/Patches.kt
@@ -370,6 +370,7 @@ private fun ExtraItem(extra: KPModel.IExtraInfo, existed: Boolean, onDelete: () 
 private fun SetSuperKeyView(viewModel: PatchesViewModel) {
     var skey by remember { mutableStateOf(viewModel.superkey) }
     var showWarn by remember { mutableStateOf(!viewModel.checkSuperKeyValidation(skey)) }
+    var charactersWarn by remember { mutableStateOf(!viewModel.checkSuperKeyCharacters(skey)) }
     var keyVisible by remember { mutableStateOf(false) }
     ElevatedCard(
         colors = CardDefaults.elevatedCardColors(containerColor = run {
@@ -399,6 +400,14 @@ private fun SetSuperKeyView(viewModel: PatchesViewModel) {
                     style = MaterialTheme.typography.bodyMedium
                 )
             }
+            if (charactersWarn) {
+                Spacer(modifier = Modifier.height(3.dp))
+                Text(
+                    color = Color.Red,
+                    text = stringResource(id = R.string.patch_item_set_skey_invalid_label),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
             Column {
                 //Spacer(modifier = Modifier.height(8.dp))
                 Box(
@@ -421,6 +430,14 @@ private fun SetSuperKeyView(viewModel: PatchesViewModel) {
                             } else {
                                 viewModel.superkey = ""
                                 showWarn = true
+                            }
+
+                            if (viewModel.checkSuperKeyCharacters(it)) {
+                                viewModel.superkey = it
+                                charactersWarn = false
+                            } else {
+                                viewModel.superkey = ""
+                                charactersWarn = true
                             }
                         },
                     )
