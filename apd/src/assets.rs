0185e9bb29bd72e2ebc19f6fa66b9bab9c70f96d
GarfieldHan <2652609017@qq.com>
2024-10-05 12:41:33 +0800

    apd: No need to ensure magiskboot
    apd doesn't use magiskboot at all
    
    Signed-off-by: GarfieldHan <2652609017@qq.com>

--- apd/src/assets.rs
+++ apd/src/assets.rs
@@ -6,12 +6,10 @@ use crate::{defs::BINARY_DIR, utils};
 pub const RESETPROP_PATH: &str = concatcp!(BINARY_DIR, "resetprop");
 pub const BUSYBOX_PATH: &str = concatcp!(BINARY_DIR, "busybox");
 pub const MAGISKPOLICY_PATH: &str = concatcp!(BINARY_DIR, "magiskpolicy");
-pub const MAGISKBOOT_PATH: &str = concatcp!(BINARY_DIR, "magiskboot");
 
 pub fn ensure_binaries() -> Result<()> {
     utils::ensure_binary(RESETPROP_PATH)?;
     utils::ensure_binary(BUSYBOX_PATH)?;
     utils::ensure_binary(MAGISKPOLICY_PATH)?;
-    utils::ensure_binary(MAGISKBOOT_PATH)?;
     Ok(())
 }
