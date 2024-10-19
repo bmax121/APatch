f34e01600cf77125697ad84e6efad85cd71a7ddd
Light summer <93428659+lightsummer233@users.noreply.github.com>
2024-10-19 11:00:44 +0800

    manager: Edge to Edge for WebUIActivity

--- app/src/main/java/me/bmax/apatch/ui/WebUIActivity.kt
+++ app/src/main/java/me/bmax/apatch/ui/WebUIActivity.kt
@@ -4,11 +4,16 @@ import android.annotation.SuppressLint
 import android.app.ActivityManager
 import android.os.Build
 import android.os.Bundle
+import android.view.ViewGroup.MarginLayoutParams
 import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import androidx.webkit.WebViewAssetLoader
 import me.bmax.apatch.APApplication
 import me.bmax.apatch.ui.webui.SuFilePathHandler
@@ -20,7 +25,14 @@ class WebUIActivity : ComponentActivity() {
     private lateinit var webViewInterface: WebViewInterface
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        enableEdgeToEdge()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            window.isNavigationBarContrastEnforced = false
+        }
+
         super.onCreate(savedInstanceState)
+
         val moduleId = intent.getStringExtra("id")!!
         val name = intent.getStringExtra("name")!!
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
@@ -53,6 +65,16 @@ class WebUIActivity : ComponentActivity() {
         }
 
         val webView = WebView(this).apply {
+            ViewCompat.setOnApplyWindowInsetsListener(this) { view, insets ->
+                val inset = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                view.updateLayoutParams<MarginLayoutParams> {
+                    leftMargin = inset.left
+                    rightMargin = inset.right
+                    topMargin = inset.top
+                    bottomMargin = inset.bottom
+                }
+                return@setOnApplyWindowInsetsListener insets
+            }
             settings.javaScriptEnabled = true
             settings.domStorageEnabled = true
             settings.allowFileAccess = false
