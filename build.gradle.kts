7f6d0cbac527b1fb327e303b5d2de7e510581699
Admirepowered <admire1971@outlook.com>
2025-02-13 13:30:15 +0800

    kernelpatch: merge into version 0.11.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ cmaker {
     }
 }
 
-project.ext.set("kernelPatchVersion", "0.11.1-dev")
+project.ext.set("kernelPatchVersion", "0.11.2")
 
 val androidMinSdkVersion = 26
 val androidTargetSdkVersion = 35
