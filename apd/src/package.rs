2d712fc585fece5181ca866bc4103e90b02e1ed9
水灵 <132723284+shulng@users.noreply.github.com>
2025-07-20 13:16:46 +0800

    apd: Revise package Modification time (#1135)

--- apd/src/package.rs
+++ apd/src/package.rs
@@ -17,56 +17,71 @@ pub struct PackageConfig {
 }
 
 pub fn read_ap_package_config() -> Vec<PackageConfig> {
-    let mut max_retry = 3;
+    let max_retry = 5;
     for i in 0..max_retry {
         let file = match File::open("/data/adb/ap/package_config") {
             Ok(file) => file,
             Err(e) => {
                 warn!("Error opening file (attempt {}): {}", i + 1, e);
-                thread::sleep(Duration::from_millis(100));
+                thread::sleep(Duration::from_secs(1));
                 continue;
             }
         };
 
         let mut reader = csv::Reader::from_reader(file);
         let mut package_configs = Vec::new();
+        let mut success = true;
         
         for record in reader.deserialize() {
             match record {
                 Ok(config) => package_configs.push(config),
                 Err(e) => {
                     warn!("Error deserializing record: {}", e);
+                    success = false;
                     break;
                 }
             }
         }
-        return package_configs;
+        
+        if success {
+            return package_configs;
+        }
+        
+        thread::sleep(Duration::from_secs(1));
     }
     return Vec::new();
 }
 
 fn write_ap_package_config(package_configs: &[PackageConfig]) {
-    let mut max_retry = 3;
+    let max_retry = 5;
 
     for i in 0..max_retry {
         let file = match File::create("/data/adb/ap/package_config") {
             Ok(file) => file,
             Err(e) => {
                 warn!("Error creating file (attempt {}): {}", i + 1, e);
-                thread::sleep(Duration::from_millis(200));
+                thread::sleep(Duration::from_secs(1));
                 continue;
             }
         };
+        
         let mut writer = csv::Writer::from_writer(file);
+        let mut success = true;
         
         for config in package_configs {
             if let Err(e) = writer.serialize(config) {
                 warn!("Error serializing record: {}", e);
+                success = false;
                 break;
             }
         }
+        
+        if success {
+            return;
+        }
+        
+        thread::sleep(Duration::from_secs(1));
     }
-
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
