868e88b65413cc7df5851cefd935503036d4b199
水灵 <132723284+shulng@users.noreply.github.com>
2025-08-07 21:35:24 +0800

    apd: fix unauthorized apps in package_config

--- apd/src/package.rs
+++ apd/src/package.rs
@@ -113,9 +113,12 @@ pub fn synchronize_package_uid() -> io::Result<()> {
     for _ in 0..max_retry {
         match read_lines("/data/system/packages.list") {
             Ok(lines) => {
+                let lines: Vec<_> = lines.filter_map(|line| line.ok()).collect();
+                
                 let mut package_configs = read_ap_package_config();
+                
                 let system_packages: Vec<String> = lines
-                    .filter_map(|line| line.ok())
+                    .iter()
                     .filter_map(|line| line.split_whitespace().next().map(|s| s.to_string()))
                     .collect();
 
@@ -123,7 +126,7 @@ pub fn synchronize_package_uid() -> io::Result<()> {
 
                 let mut updated = false;
 
-                for line in lines.filter_map(|line| line.ok()) {
+                for line in &lines {
                     let words: Vec<&str> = line.split_whitespace().collect();
                     if words.len() >= 2 {
                         if let Ok(uid) = words[1].parse::<i32>() {
