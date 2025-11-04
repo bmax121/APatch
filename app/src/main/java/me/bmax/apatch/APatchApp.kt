641dac1e1910b0384fc9fe6bb4b230de3719a2a5
Prslc <prslc113@gmail.com>
2025-11-05 03:00:04 +0800

    manager: Replaced APK signing key
    Signed-off-by: Prslc <prslc113@gmail.com>

--- app/src/main/java/me/bmax/apatch/APatchApp.kt
+++ app/src/main/java/me/bmax/apatch/APatchApp.kt
@@ -256,7 +256,7 @@ class APApplication : Application(), Thread.UncaughtExceptionHandler {
             exitProcess(0)
         }
 
-        if (!BuildConfig.DEBUG && !verifyAppSignature("1x2twMoHvfWUODv7KkRRNKBzOfEqJwRKGzJpgaz18xk=")) {
+        if (!BuildConfig.DEBUG && !verifyAppSignature("rYls6kduvik+Ioa4lP4LCMVpiVk1EogGsB6gII8TJBg=")) {
             while (true) {
                 val intent = Intent(Intent.ACTION_DELETE)
                 intent.data = "package:$packageName".toUri()
