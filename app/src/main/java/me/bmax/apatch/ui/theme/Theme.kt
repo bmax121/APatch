4e5016d3fafe60cbead2854b9a3e5f0740b186c7
dabao1955 <dabao1955@163.com>
2025-12-10 11:44:34 +0800

    Fix missing imports

--- app/src/main/java/me/bmax/apatch/ui/theme/Theme.kt
+++ app/src/main/java/me/bmax/apatch/ui/theme/Theme.kt
@@ -9,7 +9,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
