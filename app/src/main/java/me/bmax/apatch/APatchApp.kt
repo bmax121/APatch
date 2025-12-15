942ec1fa7c13b697859087124dbcd7b7155aa9e2
Admirepowered <admire1971@outlook.com>
2025-12-15 20:12:41 +0800

    fix signtrue

--- app/src/main/java/me/bmax/apatch/APatchApp.kt
+++ app/src/main/java/me/bmax/apatch/APatchApp.kt
@@ -256,7 +256,7 @@ class APApplication : Application(), Thread.UncaughtExceptionHandler {
             exitProcess(0)
         }
 
-        if (!BuildConfig.DEBUG && !verifyAppSignature("C67dtxnWxownwYNkFumlVnhGz5uM7AxWg6TRR0zuU+k=")) {
+        if (BuildConfig.DEBUG && !verifyAppSignature("C67dtxnWxownwYNkFumlVnhGz5uM7AxWg6TRR0zuU+k=")) {
             while (true) {
                 val intent = Intent(Intent.ACTION_DELETE)
                 intent.data = "package:$packageName".toUri()
