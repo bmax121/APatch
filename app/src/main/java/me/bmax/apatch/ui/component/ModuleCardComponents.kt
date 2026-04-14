f50507277c8481678541900f1dac2ef5bb2de786
Prslc <prslc113@gmail.com>
2026-04-14 16:49:01 +0800

    manager: add undo-uninstall functionality for modules
    - add `undoRemoveModule` util and support APatch CLI command
    - update `ModuleList` and `ModuleItem` to support undoing uninstallation
    - use Snackbar with reboot action for undo feedback
    - add related string resources and icons
    - adjust module action layout: move functional buttons to the left
    
    Signed-off-by: Prslc <prslc113@gmail.com>

--- app/src/main/java/me/bmax/apatch/ui/component/ModuleCardComponents.kt
+++ app/src/main/java/me/bmax/apatch/ui/component/ModuleCardComponents.kt
@@ -3,21 +3,17 @@ package me.bmax.apatch.ui.component
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import me.bmax.apatch.R
 
@@ -44,6 +40,19 @@ fun ModuleRemoveButton(
         modifier = Modifier.size(20.dp),
         painter = painterResource(id = R.drawable.trash),
         contentDescription = stringResource(id = R.string.apm_remove)
+    )
+}
+
+@Composable
+fun ModuleUndoRemoveButton(
+    enabled: Boolean, onClick: () -> Unit
+) = FilledTonalButton(
+    onClick = onClick, enabled = enabled, contentPadding = PaddingValues(12.dp)
+) {
+    Icon(
+        modifier = Modifier.size(20.dp),
+        painter = painterResource(id = R.drawable.undo),
+        contentDescription = stringResource(id = R.string.apm_undo)
     )
 }
 
